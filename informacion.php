43656080ca174d344890bfc44ee1f1f8c8c0fbad
pololoxrubio <pololox737@gmail.com>
2024-08-06 15:59:38 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacion= $_post ['Identificacion'];
+$NomApellidos=$_post ['NomApellido'];
+$cargo=$_post ['cargo'];
+$salario=$_post ['salario'];
+echo "la identificacion del empleado es: ".Identificacion;
+echo "los nombres y apellidos del empleado son: ".NomApellido;
+echo "el cargo del empleado es: ".Cargo;
+echo "el salario del empleado es: ".salario;
+?>
