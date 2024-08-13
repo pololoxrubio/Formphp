64d3cc59c44154d71e9e3bbf6ece48e42d5cf0c9
pololoxrubio <pololox737@gmail.com>
2024-08-13 14:48:44 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,7 +1,7 @@
 <?php
 $Identificacion= $_POST ['Identificacion'];
-$NomApellidos=$_POST [ 'NomApellidos'];
-$Cargo=$_POST [ 'Cargo'];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST ['Cargo'];
 $Salario=$_POST['Salario']; 
 echo "La identificacion del Empleado es: ".Identificacion;
 echo "Los nombres y apellidos del empleado son: ". NomApellidos; 
